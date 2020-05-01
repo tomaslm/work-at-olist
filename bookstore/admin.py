a571bd1280f667ee41feadc330d56985d1437cf7
tomaslm <tomaslm@hotmail.com>
2020-05-01 19:03:04 -0300

    Creation of Book and Author models

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from bookstore.models import Author, Book
+
+admin.site.register(Book)
+admin.site.register(Author)
--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["pk"]
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    edition = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    authors = models.ManyToManyField(Author)
+
+    class Meta:
+        ordering = ["pk"]
+
+    def __str__(self):
+        return self.name
