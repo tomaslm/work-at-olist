57fc0f22cc4a716a1abf670b3f5e23bb80b1e05a
tomaslm <tomaslm@hotmail.com>
2020-05-02 00:26:00 -0300

    Inclusion of drf-yasg dependency and config for auto swagger generation

--- olist/urls.py
+++ olist/urls.py
@@ -13,10 +13,41 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Bookstore API",
+        default_version="v1",
+        description="Bookstore with books and authors",
+        contact=openapi.Contact(email="tomaslm@hotmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    url(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    url(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    url(
+        r"^redoc/$",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
+    #
     path("admin/", admin.site.urls),
     path("bookstore/", include("bookstore.urls")),
 ]
